f12d01a73dbf05a6465e9328f4418ac53d14acb4
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-02 17:23:42 -0700

    feat: Add functionality to count the number of birthdays in a given month

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -7,8 +7,18 @@ function celebrateBirthday(birthday) {
   return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`;
 }
 
+function countBirthdays(arrOfBdays, month) {
+  var birthdayCounter = 0;
+  for (var i = 0; i < arrOfBdays.length; i++) {
+    if (arrOfBdays[i].month === month) {
+      birthdayCounter = birthdayCounter + 1;
+    }
+  }
+  return birthdayCounter;
+}
+
 module.exports = {
   createBirthday,
   celebrateBirthday,
-  //   countBirthday
+  countBirthdays,
 };

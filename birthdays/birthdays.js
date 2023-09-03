c1324be4733a62ed6b777d24749e0953a7a9ffff
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-02 17:55:00 -0700

    feat: Complete functionality for 'birthdays.js' (#1)
    * feat: Add functionality to create a birthday
    
    * feat: Add functionality to celebrate a birthday
    
    * feat: Add functionality to count the number of birthdays in a given month

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,24 @@
+function createBirthday(name, month, day) {
+  var birthday = { name: name, month: month, day: day };
+  return birthday;
+}
 
+function celebrateBirthday(birthday) {
+  return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`;
+}
 
-module.exports = {  };
+function countBirthdays(arrOfBdays, month) {
+  var birthdayCounter = 0;
+  for (var i = 0; i < arrOfBdays.length; i++) {
+    if (arrOfBdays[i].month === month) {
+      birthdayCounter = birthdayCounter + 1;
+    }
+  }
+  return birthdayCounter;
+}
+
+module.exports = {
+  createBirthday,
+  celebrateBirthday,
+  countBirthdays,
+};

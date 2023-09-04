75439a5d21a1bf7cde5b735a70513b732fd9f12a
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-04 16:29:20 -0700

    feat: Complete functionality for video-games.js

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,32 @@
+function createPlayer(name, age, moveset) {
+	var player = { name: name, age: age, moveset: moveset };
+	return player;
+}
 
+function createLevel(name, players) {
+	var level = { name: name, players: players, coins: 0, lives: 3 };
+	return level;
+}
 
+function findCoins(level, foundCoins) {
+	level.coins = level.coins + foundCoins;
+	if (level.coins >= 100) {
+		level.lives++;
+	}
+	return level;
+}
 
-module.exports = { };
+function defeatPlayer(level) {
+	level.lives--;
+	if (level.lives === 0) {
+		return 'GAME OVER';
+	}
+	return level;
+}
+
+module.exports = {
+	createPlayer,
+	createLevel,
+	findCoins,
+	defeatPlayer
+};

37f93bb6dec5986013aac84b3cf79151e615552d
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-06 22:20:07 -0700

    feat: Add functionality to create ingredients and create tacos

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,18 @@
+function createIngredient(name = 'unknown', price = 0.00) {
+	var ingredient = {name: name, price: price};
+	return ingredient;
+}
 
-module.exports = {}
+function createTaco(name = 'custom', ingredients = []) {
+	var taco = {name: name, ingredients: ingredients};
+	return taco;
+}
+
+
+
+module.exports = {
+  createIngredient,
+	createTaco,
+	// addIngredientToTaco,
+	// calculatePrice
+}

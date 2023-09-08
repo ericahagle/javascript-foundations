0776552583e1741dd512dfdb6b94bcd1572ace1f
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-07 21:54:27 -0700

    feat: Complete functionality for mealPlanning.js

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,30 @@
+function createMeal(type, calorieGoal, dishes) {
+	return {
+		type: type,
+		calorieGoal: calorieGoal,
+		dishes: dishes || []
+	};
+}
 
-module.exports = { }
+function addDish(meal, dishes) {
+	if (dishes.calories <= meal.calorieGoal) {
+		meal.dishes.push(dishes);
+		meal.calorieGoal = meal.calorieGoal - dishes.calories;
+	};
+	return meal;
+}
+
+function calculateCalories(meal) {
+	var totalCalories = 0;
+	for (var i = 0; i < meal.dishes.length; i++){
+		totalCalories = totalCalories + meal.dishes[i].calories;
+	};
+	return `${meal.type} has a total of ${totalCalories} calories.`;
+}
+
+
+module.exports = {
+	createMeal,
+	addDish,
+	calculateCalories
+}
